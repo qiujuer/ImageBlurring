8f19e29b09147055859673119fa47b411362ff6a
qiujuer <qiujuer@live.cn>
2016-01-10 23:51:20 +0800

    Change the sample and blur logic

--- app/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH		:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := ImageBlur
-LOCAL_SRC_FILES := net_qiujuer_imageblurring_jni_ImageBlur.cpp
-LOCAL_LDLIBS    := -lm -llog -ljnigraphics
-
-include $(BUILD_SHARED_LIBRARY)
