5078bd707d2dd4edf6ef7b603e1dea163f372684
qiujuer <qiujuer@live.cn>
2016-05-06 13:15:36 +0800

    Fixed run ndk-build error #7

--- blur/src/main/jni/Android.mk
+++ blur/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH		:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := genius_blur
-LOCAL_SRC_FILES := net_qiujuer_genius_blur_StackNative.c load.c
+LOCAL_SRC_FILES := stackblur.c net_qiujuer_genius_blur_StackNative.c load.c
 LOCAL_LDLIBS    := -lm -llog -ljnigraphics
 
 include $(BUILD_SHARED_LIBRARY)
